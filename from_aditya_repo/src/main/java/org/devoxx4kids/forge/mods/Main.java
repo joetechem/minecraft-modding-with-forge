05b3c809d0f112d147ac44c18d571e0d433c4236
Tech Em Admin <te-admin@techem-mb-4.local>
2017-06-05 11:05:15 -0400

    minor change to version call

--- from_aditya_repo/src/main/java/org/devoxx4kids/forge/mods/Main.java
+++ from_aditya_repo/src/main/java/org/devoxx4kids/forge/mods/Main.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
 	public static final String MODID = "MyMods";
-	public static final String VERSION = "2.0";
+	public static final String VERSION = "1.0";
 
 	public static Block enderBlock;
 	public static Item enderIngot;
